938d9f135de5a40446b6f5795ce988dd3a82f30e
nelermont <nelermont@yandex.ru>
2022-05-08 17:42:14 +0300

    Initial Commit

---
+++ ReadWrite_Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadWrite_Firebase
+//
+//  Created by Дмитрий Подольский on 08.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
